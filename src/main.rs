5534ed6d76edd478663290a17d4d399e85446d1d
dorayakikun <amakunai.dorayaki@gmail.com>
2017-02-11 17:31:08 +0900

    :sparkles: Make possible to recevie commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate toml;
+
+mod config;
+mod workflow;
+
+use clap::{App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let _ = workflow::new();
+    let matches = App::new("jira")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Alfred JIRA Workflow.")
+        .subcommand(SubCommand::with_name("Search")
+            .about("Search JIRA issues"))
+        .get_matches();
 }
