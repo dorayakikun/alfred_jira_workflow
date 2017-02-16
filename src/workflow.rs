47c55cfc7c981dfeca6fc8a65d5381cdaa372470
dorayakikun <amakunai.dorayaki@gmail.com>
2017-02-16 23:35:03 +0900

    :art: Improve error handling

--- src/workflow.rs
+++ src/workflow.rs
@@ -1,4 +1,5 @@
 extern crate toml;
+
 use config::Config;
 use std::env;
 use std::fs::File;
@@ -19,7 +20,7 @@ pub fn new() -> Workflow {
     let config = match load_config() {
         Ok(c) => c,
         Err(e) => {
-            println!("cause: {}", e);
+            println!("{}", e);
             process::exit(1);
         }
     };
@@ -31,11 +32,12 @@ pub fn new() -> Workflow {
 fn load_config() -> Result<Config, String> {
     let mut config_file = env::home_dir().unwrap();
     config_file.push(".jiraconfig");
-    let mut file = File::open(&config_file).expect("open config file");
+    let mut file = File::open(&config_file)
+        .map_err(|_| "Missing .jiraconfig".to_string())?;
 
     let mut toml_string = String::new();
     file.read_to_string(&mut toml_string).unwrap();
 
     toml::from_str::<Config>(&toml_string)
-        .map_err(|e| e.to_string())
+        .map_err(|e| format!("Invalid format {}", e))
 }
