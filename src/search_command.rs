531d106de01c7837703b78709899bf8e92e34d1f
dorayakikun <amakunai.dorayaki@gmail.com>
2017-02-27 23:26:55 +0900

    :art: Make possible to write alfred items

--- src/search_command.rs
+++ src/search_command.rs
@@ -4,18 +4,27 @@ use config::Config;
 use jira_api::SearchIssue;
 use jira_client;
 use search_response::SearchResponse;
+use std::io::{self, Write};
 
-struct SearchCommand {
+pub struct SearchCommand {
     pub config: Config,
 }
 
 impl SearchCommand {
-    pub fn run(&self, keyword: &String) -> Result<String, String> {
+    pub fn run(&self, keyword: &String) -> Result<(), String> {
         let jira_search = SearchIssue{
             config: self.config.clone(),
             keyword: keyword.to_string(),
         };
-        let issues = jira_client::send::<SearchIssue, SearchResponse>(jira_search).map_err(|e| e.to_string())?;
-        Ok("".to_string())
+        let res = jira_client::send::<SearchIssue, SearchResponse>(jira_search).map_err(|e| e.to_string())?;
+        Ok(self.write_alfred_items(res))
+    }
+
+    fn write_alfred_items(&self, res: SearchResponse) {
+        let alfred_items = res.issues.into_iter()
+            .map(|issue| alfred::ItemBuilder::new(issue.key).arg(issue.url).into_item())
+            .collect::<Vec<alfred::Item>>();
+
+        alfred::json::write_items(io::stdout(), &alfred_items);
     }
 }
