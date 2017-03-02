8e0dc24233cf54be3de2cb3f6e072a759e455ad6
dorayakikun <amakunai.dorayaki@gmail.com>
2017-03-02 22:44:12 +0900

    :art: Fix property names

--- src/search_response.rs
+++ src/search_response.rs
@@ -2,10 +2,10 @@ use issue::Issue;
 
 #[derive(Deserialize)]
 pub struct SearchResponse {
-    pub expand: String,
     #[serde(rename = "startAt")]
     pub start_at: i64,
     #[serde(rename = "maxResult")]
-    pub max_result: i64,
+    pub max_results: i64,
+    pub total: i64,
     pub issues: Vec<Issue>,
 }
