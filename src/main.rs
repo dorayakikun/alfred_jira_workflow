9a988181da322ed03abee9ec61c2b7c4875c457c
dorayakikun <amakunai.dorayaki@gmail.com>
2017-04-20 23:40:42 +0900

    :sparkles: Add error-chain

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+#![recursion_limit = "1024"]
+
 extern crate clap;
+#[macro_use]
+extern crate error_chain;
 extern crate hyper;
 extern crate hyper_native_tls;
 #[macro_use]
@@ -8,6 +12,9 @@ extern crate toml;
 
 mod alfred_result;
 mod config;
+mod errors {
+    error_chain!{}
+}
 mod fields;
 mod issue;
 mod item;
@@ -26,17 +33,21 @@ fn main() {
         .version(env!("CARGO_PKG_VERSION"))
         .about("Alfred JIRA Workflow.")
         .subcommand(SubCommand::with_name("search")
-            .about("Search JIRA issues")
-            .arg(Arg::with_name("keyword")
-                .required(true)))
+                        .about("Search JIRA issues")
+                        .arg(Arg::with_name("keyword").required(true)))
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("search") {
         if let Some(keyword) = matches.value_of("keyword") {
             match workflow.search().run(&keyword.to_string()) {
                 Ok(_) => (),
-                Err(e) => {
-                    println!("{}", e.to_string());
+                Err(ref e) => {
+                    for e in e.iter().skip(1) {
+                        println!("caused by: {}", e);
+                    }
+                    if let Some(backtrace) = e.backtrace() {
+                        println!("backtrace: {:?}", backtrace);
+                    }
                     std::process::exit(1);
                 }
             }
--- src/search_command.rs
+++ src/search_command.rs
@@ -1,5 +1,6 @@
 use alfred_result::AlfredResult;
 use config::Config;
+use errors::*;
 use item::Item;
 use item::ItemBuilder;
 use jira_api::SearchIssue;
@@ -13,16 +14,16 @@ pub struct SearchCommand {
 }
 
 impl SearchCommand {
-    pub fn run(&self, keyword: &String) -> Result<(), String> {
+    pub fn run(&self, keyword: &String) -> Result<()> {
         let jira_search = SearchIssue {
             config: self.config.clone(),
             keyword: keyword.to_string(),
         };
-        let res = jira_client::send::<SearchIssue>(jira_search).map_err(|e| e.to_string())?;
+        let res = jira_client::send::<SearchIssue>(jira_search).chain_err(|| "Failed to call search API".to_string())?;
         Ok(self.write_alfred_items(self.config.hostname(), res)?)
     }
 
-    fn write_alfred_items(&self, base_url: &String, res: SearchResponse) -> Result<(), String> {
+    fn write_alfred_items(&self, base_url: &String, res: SearchResponse) -> Result<()> {
         let alfred_items = res.issues
             .into_iter()
             .map(|issue| {
@@ -34,6 +35,6 @@ impl SearchCommand {
             .collect::<Vec<Item>>();
 
         let alfred_result = AlfredResult { items: alfred_items };
-        serde_json::to_writer_pretty(&mut io::stdout(), &alfred_result).map_err(|e| e.to_string())
+        serde_json::to_writer_pretty(&mut io::stdout(), &alfred_result).chain_err(|| "Failed to the serialize pretty JSON")
     }
 }
